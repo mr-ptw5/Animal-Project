8b0300a5946a6428f682002d9115a5778d6b1a35
mr-ptw5 <wrightptw@gmail.com>
2022-07-10 22:22:45 -0500

    server.js planning

--- server.js
+++ server.js
@@ -2,5 +2,30 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-    
+
 })
+
+
+
+
+
+//receive 
+/*
+bird sighting
+    coordinate?
+    bird species?
+    notes?
+    photo
+    maybe behavior
+
+new filter
+
+
+
+get requests
+get list of birds for the filter criteria
+
+get data for a bird
+    list of sightings
+
+*/
